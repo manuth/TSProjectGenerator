dd65e41853a4cc1f7ff266b62fb1b52a35dd66f3
Manuel Thalmann <m@nuth.ch>
2018-11-01 00:45:34 +0100

    Implement mocha-tests

---
+++ src/tests/main.test.ts
@@ -0,0 +1,13 @@
+import * as assert from "assert";
+
+suite(
+    "GeneratorTSGenerator",
+    () =>
+    {
+        test(
+            "Example...",
+            () =>
+            {
+                assert.deepEqual(1, 1);
+            });
+    });
