9fab8c0c1e8c8a48f14edc07fd60589021e4ab08
Manuel Thalmann <m@nuth.ch>
2018-11-07 13:55:52 +0100

    Fix the console-log

--- src/generators/app/index.ts
+++ src/generators/app/index.ts
@@ -202,13 +202,13 @@ class AppGenerator extends Generator<IAppSettings>
         this.fs.copy(Path.join(moduleRoot, sourceRoot, "IFileMapping.ts"), Path.join(sourceRoot, "IFileMapping.ts"));
         this.fs.copy(Path.join(moduleRoot, sourceRoot, "IGeneratorSettings.ts"), Path.join(sourceRoot, "IGeneratorSettings.ts"));
         FileSystem.ensureDir(this.destinationPath(sourceRoot, "generators"));
-        await super.writing();
-        this.log("Your workspace has been generated!");
-        this.log();
+        return super.writing();
     }
 
     public async install()
     {
+        this.log("Your workspace has been generated!");
+        this.log();
         this.log(chalk.whiteBright("Installing dependencies..."));
         this.npmInstall();
     }
