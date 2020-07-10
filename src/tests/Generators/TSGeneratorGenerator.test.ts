897ff66a778a69369e30557a986f375e6cabc04e
Manuel Thalmann <m@nuth.ch>
2020-07-10 02:53:50 +0200

    Use `extended-yo-generator-test` for testing

--- src/tests/Generators/TSGeneratorGenerator.test.ts
+++ src/tests/Generators/TSGeneratorGenerator.test.ts
@@ -2,43 +2,50 @@ import Assert = require("assert");
 import { spawnSync } from "child_process";
 import Path = require("path");
 import { GeneratorSettingKey } from "@manuth/extended-yo-generator";
+import { TestContext, IRunContext } from "@manuth/extended-yo-generator-test";
 import npmWhich = require("npm-which");
-import { run, RunContext } from "yeoman-test";
+import { TempDirectory } from "temp-filesystem";
+import { run } from "yeoman-test";
 import { LintRuleset } from "../../Linting/LintRuleset";
 import { TSGeneratorComponent } from "../../generators/app/TSGeneratorComponent";
+import { TSGeneratorGenerator } from "../../generators/app/TSGeneratorGenerator";
 import { TSGeneratorSettingKey } from "../../generators/app/TSGeneratorSettingKey";
 
 suite(
     "TSGenerator-Generator",
     () =>
     {
+        let generatorDir: TempDirectory;
         let currentDir: string;
-        let generatorDir: string;
-        let runContext: RunContext;
+        let testContext: TestContext<TSGeneratorGenerator>;
+        let runContext: IRunContext<TSGeneratorGenerator>;
         let generatorName: string;
 
         suiteSetup(
             () =>
             {
+                generatorDir = new TempDirectory();
                 currentDir = process.cwd();
                 generatorName = "generator-test";
+                testContext = new TestContext(Path.join(__dirname, "..", "..", "generators", "app"));
 
-                runContext = run(
-                    Path.join(__dirname, "..", "..", "generators", "app")).withPrompts(
+                runContext = testContext.ExecuteGenerator().withPrompts(
+                    {
+                        [TSGeneratorSettingKey.Destination]: "./",
+                        [TSGeneratorSettingKey.DisplayName]: generatorName,
+                        [TSGeneratorSettingKey.Name]: generatorName,
+                        [GeneratorSettingKey.Components]: [
+                            TSGeneratorComponent.Linting,
+                            TSGeneratorComponent.VSCode,
+                            TSGeneratorComponent.GeneratorExample
+                        ],
+                        [TSGeneratorSettingKey.LintRuleset]: LintRuleset.Weak
+                    }).withOptions(
                         {
-                            [TSGeneratorSettingKey.Destination]: "./",
-                            [TSGeneratorSettingKey.DisplayName]: generatorName,
-                            [TSGeneratorSettingKey.Name]: generatorName,
-                            [GeneratorSettingKey.Components]: [
-                                TSGeneratorComponent.Linting,
-                                TSGeneratorComponent.VSCode,
-                                TSGeneratorComponent.GeneratorExample
-                            ],
-                            [TSGeneratorSettingKey.LintRuleset]: LintRuleset.Weak
-                        }).withOptions(
-                            {
-                                "skip-install": false
-                            });
+                            "skip-install": false
+                        }).inDir(
+                            generatorDir.FullName
+                        );
             });
 
         suiteTeardown(
@@ -47,64 +54,68 @@ suite(
                 this.timeout(60 * 1000);
                 this.slow(30 * 1000);
                 process.chdir(currentDir);
-                runContext.cleanTestDirectory();
             });
 
-        test(
-            "Checking whether the generator can be executed…",
-            async function()
+        suite(
+            "General",
+            () =>
             {
-                this.timeout(7 * 60 * 1000);
-                this.slow(6.5 * 60 * 1000);
-                generatorDir = await runContext.toPromise();
-            });
-
-        test(
-            "Checking whether the generated module can be installed…",
-            function()
-            {
-                this.timeout(6.5 * 60 * 1000);
-                this.slow(3.25 * 60 * 1000);
-
-                let result = spawnSync(
-                    npmWhich(__dirname).sync("npm"),
-                    [
-                        "install",
-                        "--silent"
-                    ],
+                test(
+                    "Checking whether the generator can be executed…",
+                    async function()
                     {
-                        cwd: generatorDir
+                        this.timeout(7 * 60 * 1000);
+                        this.slow(6.5 * 60 * 1000);
+                        return Assert.doesNotReject(async () => runContext.toPromise());
                     });
 
-                Assert.strictEqual(result.status === 0, true);
-            });
+                test(
+                    "Checking whether the generated module can be installed…",
+                    function()
+                    {
+                        this.timeout(6.5 * 60 * 1000);
+                        this.slow(3.25 * 60 * 1000);
 
-        test(
-            "Checking whether the generated module can be compiled using typescript…",
-            async function()
-            {
-                this.timeout(15.5 * 1000);
-                this.slow(7.75 * 1000);
+                        let result = spawnSync(
+                            npmWhich(__dirname).sync("npm"),
+                            [
+                                "install",
+                                "--silent"
+                            ],
+                            {
+                                cwd: generatorDir.FullName
+                            });
 
-                let result = spawnSync(
-                    npmWhich(__dirname).sync("tsc"),
-                    [
-                        "-p",
-                        generatorDir
-                    ]);
+                        Assert.strictEqual(result.status === 0, true);
+                    });
 
-                Assert.strictEqual(result.status === 0, true);
-            });
+                test(
+                    "Checking whether the generated module can be compiled using typescript…",
+                    async function()
+                    {
+                        this.timeout(15.5 * 1000);
+                        this.slow(7.75 * 1000);
 
-        test(
-            "Checking whether the generated module can be used as a generator…",
-            async function()
-            {
-                this.timeout(20 * 1000);
-                this.slow(10 * 1000);
-                let testContext = run(Path.join(generatorDir, "lib", "generators", "app"));
-                await Assert.doesNotReject(testContext.toPromise());
-                process.chdir(generatorDir);
-                testContext.cleanTestDirectory();
+                        let result = spawnSync(
+                            npmWhich(__dirname).sync("tsc"),
+                            [
+                                "-p",
+                                "."
+                            ]);
+
+                        Assert.strictEqual(result.status === 0, true);
+                    });
+
+                test(
+                    "Checking whether the generated module can be used as a generator…",
+                    async function()
+                    {
+                        this.timeout(20 * 1000);
+                        this.slow(10 * 1000);
+                        let testContext = run(Path.join(generatorDir.FullName, "lib", "generators", "app"));
+                        await Assert.doesNotReject(testContext.toPromise());
+                        process.chdir(currentDir);
+                        testContext.cleanTestDirectory();
+                    });
             });
     });
