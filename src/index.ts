b3770a4a332f803cab9460a3459fba4120e41ba1
Manuel Thalmann <m@nuth.ch>
2020-07-11 23:10:25 +0200

    Move inquiry-components to a separate directory

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ export { SubGeneratorSettingKey } from "./generators/generator/SubGeneratorSetti
 export { TSGeneratorComponent } from "./generators/generator/TSGeneratorComponent";
 export { TSGeneratorGenerator } from "./generators/generator/TSGeneratorGenerator";
 export { TSGeneratorSettingKey } from "./generators/generator/TSGeneratorSettingKey";
+export { PromptCallback } from "./Inquiry/Prompts/PromptCallback";
+export { SubGeneratorPrompt } from "./Inquiry/Prompts/SubGeneratorPrompt";
 export { LintRuleset } from "./Linting/LintRuleset";
 export { CommonDependencies } from "./NPMPackaging/CommonDependencies";
 export { GeneratorDependencies } from "./NPMPackaging/GeneratorDependencies";
 export { IScriptMapping } from "./NPMPackaging/IScriptMapping";
 export { LintDependencies } from "./NPMPackaging/LintDependencies";
-export { PromptCallback } from "./Prompting/PromptCallback";
-export { SubGeneratorPrompt } from "./Prompting/SubGeneratorPrompt";
