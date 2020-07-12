2b99ef8c1f473e64676be1485211c8461e3d82a8
Manuel Thalmann <m@nuth.ch>
2020-07-12 17:50:33 +0200

    Add a file-mapping for `tasks.json`-files in `TSProject`s

---
+++ src/Project/FileMappings/VSCode/ProjectTasksFileMapping.ts
@@ -0,0 +1,47 @@
+import { TaskDefinition } from "vscode";
+import { CodeWorkspaceComponent } from "../../Components/CodeWorkspaceComponent";
+import { ITSProjectSettings } from "../../ITSProjectSettings";
+import { VSCodeTasksFileMapping } from "./VSCodeTasksFileMapping";
+
+/**
+ * Provides a file-mapping for copying the `tasks.json` file.
+ */
+export class ProjectTasksFileMapping<T extends ITSProjectSettings> extends VSCodeTasksFileMapping<T>
+{
+    /**
+     * Initializes a new instance of the `ProjectTasksFileMapping` class.
+     *
+     * @param codeWorkspaceComponent
+     * The workspace-component of this file-mapping.
+     */
+    public constructor(codeWorkspaceComponent: CodeWorkspaceComponent<T>)
+    {
+        super(codeWorkspaceComponent);
+    }
+
+    /**
+     * Processes a task-configuration.
+     *
+     * @param task
+     * The task to process.
+     *
+     * @returns
+     * The processed task.
+     */
+    protected async ProcessTask(task: TaskDefinition): Promise<TaskDefinition>
+    {
+        if (
+            typeof task.label === "string" &&
+            task.label.toLowerCase() === "lint")
+        {
+            if (
+                typeof task.problemMatcher === "object" &&
+                Array.isArray(task.problemMatcher))
+            {
+                task.problemMatcher = task.problemMatcher[0];
+            }
+        }
+
+        return task;
+    }
+}
