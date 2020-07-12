291483cc6f1c278126496c85003e436f9ed82f0c
Manuel Thalmann <m@nuth.ch>
2020-07-12 13:56:34 +0200

    Add a file-mapping for `launch.json` files

---
+++ src/Project/FileMappings/VSCode/VSCodeLaunchFileMapping.ts
@@ -0,0 +1,114 @@
+import { FileMapping, IGenerator } from "@manuth/extended-yo-generator";
+import JSON = require("comment-json");
+import { readFile } from "fs-extra";
+import { join } from "upath";
+import { DebugConfiguration } from "vscode";
+import { ILaunchFile } from "../../../VSCode/ILaunchFile";
+import { CodeWorkspaceComponent } from "../../Components/CodeWorkspaceComponent";
+import { ITSProjectSettings } from "../../ITSProjectSettings";
+import { VSCodeWorkspaceFileMapping } from "./VSCodeWorkspaceFileMapping";
+
+/**
+ * Provides a file-mapping for copying the `launch.json` file.
+ */
+export class VSCodeLaunchFileMapping<T extends ITSProjectSettings> extends VSCodeWorkspaceFileMapping<T>
+{
+    /**
+     * Initializes a new instance of the `VSCodeLaunchFileMapping` class.
+     *
+     * @param codeWorkspaceComponent
+     * The workspace-component of this file-mapping.
+     */
+    public constructor(codeWorkspaceComponent: CodeWorkspaceComponent<T>)
+    {
+        super(codeWorkspaceComponent);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param fileMapping
+     * The target of the resolve.
+     *
+     * @param generator
+     * The generator of the target.
+     *
+     * @returns
+     * The source of the file-mapping.
+     */
+    public async Source(fileMapping: FileMapping<T>, generator: IGenerator<T>): Promise<string>
+    {
+        return generator.modulePath(this.SettingsFolderName, "launch.json");
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param fileMapping
+     * The target of the resolve.
+     *
+     * @param generator
+     * The generator of the target.
+     *
+     * @returns
+     * The destination of the file-mapping.
+     */
+    public async Destination(fileMapping: FileMapping<T>, generator: IGenerator<T>): Promise<string>
+    {
+        return join(this.SettingsFolderName, "launch.json");
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param fileMapping
+     * The target of the resolve.
+     *
+     * @param generator
+     * The generator of the target.
+     */
+    public async Processor(fileMapping: FileMapping<T>, generator: IGenerator<T>): Promise<void>
+    {
+        let result: ILaunchFile = JSON.parse((await readFile(await fileMapping.Source)).toString());
+        result.configurations = (await this.FilterSettings(result.configurations ?? []));
+
+        for (let i = 0; i < result.configurations.length; i++)
+        {
+            result.configurations[i] = await this.ProcessDebugConfig(result.configurations[i]);
+        }
+
+        generator.fs.write(await fileMapping.Destination, JSON.stringify(result, null, 4));
+    }
+
+    /**
+     * Filters the debug-settings.
+     *
+     * @param debugSettings
+     * The debug-settings to filter.
+     *
+     * @returns
+     * All necessary debug-settings.
+     */
+    protected async FilterSettings(debugSettings: DebugConfiguration[]): Promise<DebugConfiguration[]>
+    {
+        return debugSettings.filter(
+            (debugConfiguration) =>
+            {
+                return !debugConfiguration.name.toLowerCase().includes("yeoman");
+            });
+    }
+
+    /**
+     * Processes a debug-configuration.
+     *
+     * @param debugConfig
+     * The debug-configuration to process.
+     *
+     * @returns
+     * The processed debug-configuration.
+     */
+    protected async ProcessDebugConfig(debugConfig: DebugConfiguration): Promise<DebugConfiguration>
+    {
+        return debugConfig;
+    }
+}
