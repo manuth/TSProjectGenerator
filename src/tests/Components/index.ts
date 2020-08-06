8b8893c5883a991f1c75f926da726d692a489b48
Manuel Thalmann <m@nuth.ch>
2020-08-06 23:46:32 +0200

    Remove unnecessary function

--- src/tests/Components/index.ts
+++ src/tests/Components/index.ts
@@ -1,7 +1,4 @@
-import { IFileMapping } from "@manuth/extended-yo-generator";
 import { TestContext, TestGenerator, ITestOptions, ITestGeneratorOptions } from "@manuth/extended-yo-generator-test";
-import { ComponentBase } from "../../Components/ComponentBase";
-import { FileMappingConstructor } from "./FileMappingContrsuctor";
 import { TransformationTests } from "./Transformation";
 
 /**
@@ -18,27 +15,4 @@ export function ComponentTests(context: TestContext<TestGenerator, ITestGenerato
         {
             TransformationTests(context);
         });
-}
-
-/**
- * Searches the specified `component` for a file-mapping with the specified type.
- *
- * @param component
- * The component to search.
- *
- * @param fileMappingType
- * The type of file-mapping to find.
- *
- * @returns
- * The file-mapping with the specified type.
- */
-export async function GetComponentFileMapping<TFileMapping extends IFileMapping<any>>(component: ComponentBase<any>, fileMappingType: FileMappingConstructor<any>): Promise<TFileMapping>
-{
-    let fileMapping = (await component.FileMappings).find(
-        (fileMapping) =>
-        {
-            return fileMapping instanceof fileMappingType;
-        });
-
-    return fileMapping as any;
 }
