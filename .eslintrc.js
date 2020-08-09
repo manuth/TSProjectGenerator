8ca3b0192a2213809d98939c65050f045bd2b17f
Manuel Thalmann <m@nuth.ch>
2020-08-09 17:21:15 +0200

    Remove redundant eslint-configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,12 @@ const path = require("path");
 
 module.exports = {
     extends: [
-        "plugin:@manuth/typescript/recommended-requiring-type-checking"
+        require.resolve("./packages/generator-ts-project/.eslintrc.js")
     ],
-    env: {
-        node: true,
-        es6: true
-    },
     parserOptions: {
         project: [
+            path.join(__dirname, "tsconfig.json"),
             path.join(__dirname, "tsconfig.eslint.json")
-        ]
-    },
-    rules: {
-        "@typescript-eslint/explicit-module-boundary-types": [
-            "warn",
-            {
-                allowArgumentsExplicitlyTypedAsAny: true
-            }
-        ],
-        "@typescript-eslint/no-dynamic-delete": "off",
-        "@typescript-eslint/no-unused-vars": [
-            "warn",
-            {
-                args: "none"
-            }
         ]
     }
 };
