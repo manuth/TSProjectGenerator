d9b2adc61ea1cf130a3c37cc2b97b26c185f2ac3
Manuel Thalmann <m@nuth.ch>
2020-08-11 21:16:15 +0200

    Add further tests for the `TSProjectTasksProcessor`

--- packages/generator-ts-project/src/tests/Project/VSCode/TSProjectTasksProcessor.test.ts
+++ packages/generator-ts-project/src/tests/Project/VSCode/TSProjectTasksProcessor.test.ts
@@ -1,5 +1,6 @@
 import Assert = require("assert");
 import { TestContext } from "@manuth/extended-yo-generator-test";
+import { TaskDefinition } from "vscode";
 import { TSProjectWorkspaceFolder } from "../../../Project/Components/TSProjectCodeWorkspaceComponent";
 import { ITSProjectSettings } from "../../../Project/Settings/ITSProjectSettings";
 import { TSProjectGenerator } from "../../../Project/TSProjectGenerator";
@@ -19,6 +20,8 @@ export function TSProjectTasksProcessorTests(context: TestContext<TSProjectGener
         {
             let component: TSProjectWorkspaceFolder<ITSProjectSettings>;
             let processor: TSProjectTasksProcessor<ITSProjectSettings>;
+            let workspaceDirective: string;
+            let namedWorkspaceDirective: string;
 
             suiteSetup(
                 async function()
@@ -26,7 +29,29 @@ export function TSProjectTasksProcessorTests(context: TestContext<TSProjectGener
                     this.timeout(0);
                     component = new TSProjectWorkspaceFolder(await context.Generator);
                     processor = new TSProjectTasksProcessor(component);
+                    workspaceDirective = "${workspaceFolder}";
+                    namedWorkspaceDirective = "${workspaceFolder:Test}";
                 });
+
+            /**
+             * Processes the specified `task`.
+             *
+             * @param task
+             * The task to process.
+             *
+             * @returns
+             * The processed task.
+             */
+            async function ProcessTask(task: TaskDefinition): Promise<TaskDefinition>
+            {
+                return (await processor.Process(
+                    {
+                        version: "",
+                        tasks: [
+                            task
+                        ]
+                    })).tasks[0];
+            }
 
             test(
                 "Checking whether the problem-matcher of the `lint` task is correct…",
@@ -42,6 +67,212 @@ export function TSProjectTasksProcessorTests(context: TestContext<TSProjectGener
                     Assert.ok(
                         typeof lintTask.problemMatcher === "string" &&
                         lintTask.problemMatcher.startsWith("$eslint"));
+                });
+
+            test(
+                "Checking whether shell-scripts are converted to npm-scripts…",
+                async () =>
+                {
+                    Assert.deepStrictEqual(
+                        await ProcessTask(
+                            {
+                                type: "shell",
+                                command: "npm",
+                                args: [
+                                    "run",
+                                    "lint"
+                                ]
+                            }),
+                        {
+                            type: "npm",
+                            script: "lint"
+                        });
+                });
+
+            test(
+                "Checking whether shell-scripts with unspecified npm-script names aren't converted…",
+                async () =>
+                {
+                    let task = {
+                        type: "shell",
+                        command: "npm",
+                        args: [
+                            "run"
+                        ]
+                    };
+
+                    Assert.deepStrictEqual(await ProcessTask(task), task);
+                });
+
+            test(
+                "Checking whether arguments for the npm-scripts are preserved…",
+                async () =>
+                {
+                    let args = [
+                        "--target",
+                        "Release"
+                    ];
+
+                    Assert.deepStrictEqual(
+                        await ProcessTask(
+                            {
+                                type: "shell",
+                                command: "npm",
+                                args: [
+                                    "run",
+                                    "build",
+                                    ...args
+                                ]
+                            }),
+                        {
+                            type: "npm",
+                            script: "build",
+                            args
+                        });
+                });
+
+            test(
+                "Checking whether inexistent properties aren't added to the task…",
+                async () =>
+                {
+                    let task = {
+                        type: "shell",
+                        command: "npm",
+                        args: [
+                            "run",
+                            "build"
+                        ]
+                    };
+
+                    Assert.ok(!("label" in await ProcessTask(task)));
+                });
+
+            test(
+                "Checking whether named workspace-folder directives are stripped from `options.cwd`…",
+                async () =>
+                {
+                    /**
+                     * Creates a new cwd-path.
+                     *
+                     * @param root
+                     * The root of the cwd.
+                     *
+                     * @returns
+                     * A new cwd-path.
+                     */
+                    let cwdCreator = (root: string): string => `${root}/src?`;
+
+                    Assert.strictEqual(
+                        (await ProcessTask(
+                            {
+                                type: "",
+                                options: {
+                                    cwd: cwdCreator(namedWorkspaceDirective)
+                                }
+                            })).options.cwd,
+                        cwdCreator(workspaceDirective));
+                });
+
+            test(
+                "Checking whether `cwd`s pointing to the workspace-folder are removed…",
+                async () =>
+                {
+                    Assert.ok(
+                        !("cwd" in (await ProcessTask(
+                            {
+                                type: "",
+                                options: {
+                                    cwd: namedWorkspaceDirective,
+                                    test: {}
+                                }
+                            })).options));
+                });
+
+            test(
+                "Checking whether `options` are deleted if left empty…",
+                async () =>
+                {
+                    Assert.ok(
+                        !("options" in (await ProcessTask(
+                            {
+                                type: "",
+                                options: {}
+                            }))));
+
+                    Assert.ok(
+                        !("options" in (await ProcessTask(
+                            {
+                                type: "",
+                                options: {
+                                    cwd: namedWorkspaceDirective
+                                }
+                            }))));
+                });
+
+            test(
+                "Checking whether unnecessary `fileLocation` options are deleted…",
+                async () =>
+                {
+                    Assert.ok(
+                        !("fileLocation" in (await ProcessTask(
+                            {
+                                type: "",
+                                problemMatcher: {
+                                    test: "",
+                                    fileLocation: [
+                                        "relative",
+                                        namedWorkspaceDirective
+                                    ]
+                                }
+                            })).problemMatcher));
+                });
+
+            test(
+                "Checking whether problem-matchers only containing a `base` are processed correctly…",
+                async () =>
+                {
+                    let base = "$eslint-compact";
+
+                    Assert.strictEqual(
+                        (await ProcessTask(
+                            {
+                                type: "",
+                                problemMatcher: {
+                                    base
+                                }
+                            })).problemMatcher,
+                        base);
+
+                    Assert.strictEqual(
+                        (await ProcessTask(
+                            {
+                                type: "",
+                                problemMatcher: {
+                                    fileLocation: [
+                                        "relative",
+                                        namedWorkspaceDirective
+                                    ],
+                                    base
+                                }
+                            })).problemMatcher,
+                        base);
+                });
+
+            test(
+                "Checking whether unnecessary problemMatcher-arrays are simplified…",
+                async () =>
+                {
+                    let problemMatcher = "$eslint-compact";
+
+                    Assert.strictEqual(
+                        (await ProcessTask(
+                            {
+                                type: "",
+                                problemMatcher: [
+                                    problemMatcher
+                                ]
+                            })).problemMatcher,
+                        problemMatcher);
                 });
         });
 }
