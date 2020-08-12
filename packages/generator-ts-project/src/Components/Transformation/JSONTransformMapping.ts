b93d853541064d6ae49a7ac534229a3cc0ffbe8e
Manuel Thalmann <m@nuth.ch>
2020-08-12 20:11:03 +0200

    Add file-mappings for transforming objects

---
+++ packages/generator-ts-project/src/Components/Transformation/JSONTransformMapping.ts
@@ -0,0 +1,48 @@
+import { IGeneratorSettings, IGenerator } from "@manuth/extended-yo-generator";
+import JSON = require("comment-json");
+import { ObjectTransformMapping } from "./ObjectTransformMapping";
+
+/**
+ * Provides the functionality to transform and copy JSON-code.
+ */
+export abstract class JSONTransformMapping<TData, TSettings extends IGeneratorSettings> extends ObjectTransformMapping<TData, TSettings>
+{
+    /**
+     * Initializes a new instance of the `JSONTransformMapping` class.
+     *
+     * @param generator
+     * The generator of the file-mapping.
+     */
+    public constructor(generator: IGenerator<TSettings>)
+    {
+        super(generator);
+    }
+
+    /**
+     * Loads the meta-data from the `text`.
+     *
+     * @param text
+     * The text representing the meta-data.
+     *
+     * @returns
+     * An object loaded from the `text`.
+     */
+    protected async Parse(text: string): Promise<TData>
+    {
+        return JSON.parse(text);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param metadata
+     * The metadata to dump.
+     *
+     * @returns
+     * A text representing the `metadata`.
+     */
+    protected async Dump(metadata: TData): Promise<string>
+    {
+        return JSON.stringify(metadata);
+    }
+}
---
+++ packages/generator-ts-project/src/Components/Transformation/YAMLTransformMapping.ts
@@ -0,0 +1,48 @@
+import { IGeneratorSettings, IGenerator } from "@manuth/extended-yo-generator";
+import { safeDump, safeLoadAll } from "js-yaml";
+import { ObjectTransformMapping } from "./ObjectTransformMapping";
+
+/**
+ * Provides the functionality to transform and copy YAML-code.
+ */
+export abstract class YAMLTransformMapping<TData extends any[], TSettings extends IGeneratorSettings> extends ObjectTransformMapping<TData, TSettings>
+{
+    /**
+     * Initializes a new instance of the `YAMLTransformMapping` class.
+     *
+     * @param generator
+     * The generator of the file-mapping.
+     */
+    public constructor(generator: IGenerator<TSettings>)
+    {
+        super(generator);
+    }
+
+    /**
+     * Loads the meta-data from the `text`.
+     *
+     * @param text
+     * The text representing the meta-data.
+     *
+     * @returns
+     * An object loaded from the `text`.
+     */
+    protected async Parse(text: string): Promise<TData>
+    {
+        return safeLoadAll(text) as TData;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param metadata
+     * The metadata to dump.
+     *
+     * @returns
+     * A text representing the `metadata`.
+     */
+    protected async Dump(metadata: TData): Promise<string>
+    {
+        return metadata.map((document) => safeDump(document)).join("---\n");
+    }
+}
