6091861caaad8c8693c422c62d3a6ae5a4048f85
Manuel Thalmann <m@nuth.ch>
2020-08-15 20:05:41 +0200

    Move from js-yaml to yaml

--- packages/generator-ts-project/src/Components/Transformation/YAMLTransformMapping.ts
+++ packages/generator-ts-project/src/Components/Transformation/YAMLTransformMapping.ts
@@ -1,11 +1,11 @@
 import { IGeneratorSettings, IGenerator } from "@manuth/extended-yo-generator";
-import { safeDump, safeLoadAll } from "js-yaml";
+import { parseAllDocuments, Document } from "yaml";
 import { TransformFileMapping } from "./TransformFileMapping";
 
 /**
  * Provides the functionality to transform and copy YAML-code.
  */
-export abstract class YAMLTransformMapping<TSettings extends IGeneratorSettings, TData extends any[]> extends TransformFileMapping<TSettings, TData>
+export abstract class YAMLTransformMapping<TSettings extends IGeneratorSettings> extends TransformFileMapping<TSettings, Document.Parsed[]>
 {
     /**
      * Initializes a new instance of the `YAMLTransformMapping` class.
@@ -27,9 +27,9 @@ export abstract class YAMLTransformMapping<TSettings extends IGeneratorSettings,
      * @returns
      * An object loaded from the `text`.
      */
-    protected async Parse(text: string): Promise<TData>
+    protected async Parse(text: string): Promise<Document.Parsed[]>
     {
-        return safeLoadAll(text) as TData;
+        return parseAllDocuments(text);
     }
 
     /**
@@ -41,8 +41,8 @@ export abstract class YAMLTransformMapping<TSettings extends IGeneratorSettings,
      * @returns
      * A text representing the `metadata`.
      */
-    protected async Dump(metadata: TData): Promise<string>
+    protected async Dump(metadata: Document.Parsed[]): Promise<string>
     {
-        return metadata.map((document) => safeDump(document)).join("---\n");
+        return metadata.map((document) => document.toString()).join("---\n");
     }
 }
