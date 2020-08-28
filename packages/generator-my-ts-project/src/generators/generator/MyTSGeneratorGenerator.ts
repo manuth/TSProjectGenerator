764de2d6fb0123bd0217964cc54d873648da1a4b
Manuel Thalmann <m@nuth.ch>
2020-08-28 20:23:57 +0200

    Add a custom TSGeneratorGenerator

---
+++ packages/generator-my-ts-project/src/generators/generator/MyTSGeneratorGenerator.ts
@@ -0,0 +1,63 @@
+import { GeneratorOptions } from "@manuth/extended-yo-generator";
+import { ITSGeneratorSettings, TSGeneratorGenerator } from "@manuth/generator-ts-project";
+import { MyTSProjectGenerator } from "../../MyTSProjectGenerator";
+
+/**
+ * Provides the functionality to generate a custom generator written in TypeScript.
+ */
+export class MyTSGeneratorGenerator<TSettings extends ITSGeneratorSettings = ITSGeneratorSettings, TOptions extends GeneratorOptions = GeneratorOptions> extends MyTSProjectGenerator.Create(TSGeneratorGenerator, require.resolve("@manuth/generator-ts-project"))<TSettings, TOptions>
+{
+    /**
+     * Initializes a new instance of the `MyTSGeneratorGenerator` class.
+     *
+     * @param args
+     * A set of arguments for the generator.
+     *
+     * @param options
+     * A set of options for the generator.
+     */
+    public constructor(args: string | string[], options: TOptions)
+    {
+        super(args, options);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public async prompting(): Promise<void>
+    {
+        return super.prompting();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public async writing(): Promise<void>
+    {
+        return super.writing();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public async install(): Promise<void>
+    {
+        return super.install();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public async cleanup(): Promise<void>
+    {
+        return super.cleanup();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public async end(): Promise<void>
+    {
+        return super.end();
+    }
+}
