3046f6a81de1e0fcfd992a6ad795c4ef2df21286
Manuel Thalmann <m@nuth.ch>
2020-08-29 12:08:32 +0200

    Add tests for the drone-filemapping

--- packages/generator-my-ts-project/src/tests/main.test.ts
+++ packages/generator-my-ts-project/src/tests/main.test.ts
@@ -1,4 +1,7 @@
 import { TestContext } from "@manuth/extended-yo-generator-test";
+import { join } from "upath";
+import { MyTSModuleGenerator } from "../generators/module/MyTSModuleGenerator";
+import { DroneFileMappingTests } from "./DroneFileMapping.test";
 import { MarkdownFileProcessorTests } from "./MarkdownFileProcessor.test";
 
 suite(
@@ -6,5 +9,17 @@ suite(
     () =>
     {
         let context = TestContext.Default;
+        let projectContext = new TestContext<MyTSModuleGenerator>(join(__dirname, "..", "generators", "module"));
+
+        suiteTeardown(
+            () =>
+            {
+                for (let testContext of [context, projectContext])
+                {
+                    testContext.Dispose();
+                }
+            });
+
         MarkdownFileProcessorTests(context);
+        DroneFileMappingTests(projectContext);
     });
