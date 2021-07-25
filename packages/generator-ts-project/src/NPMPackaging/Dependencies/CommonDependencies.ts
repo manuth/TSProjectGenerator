1f67482f8fa2e5d29f560e726cc6f121184d1af1
Manuel Thalmann <m@nuth.ch>
2021-07-25 14:33:28 +0200

    Fix broken `CommonDependencies`-class

--- packages/generator-ts-project/src/NPMPackaging/Dependencies/CommonDependencies.ts
+++ packages/generator-ts-project/src/NPMPackaging/Dependencies/CommonDependencies.ts
@@ -1,27 +1,36 @@
+import { PackageDependencyCollectionOptions } from "@manuth/package-json-editor";
 import { BuildDependencies } from "./BuildDependencies";
-import { MyPackageDependencyCollection } from "./MyPackageDependencyCollection";
 
 /**
  * Provides all common dependencies.
  */
-export class CommonDependencies extends MyPackageDependencyCollection
+export class CommonDependencies extends BuildDependencies
 {
     /**
      * Initializes a new instance of the {@link CommonDependencies `CommonDependencies`} class.
      */
     public constructor()
     {
-        super(
-            {
-                devDependencies: [
-                    "@types/mocha",
-                    "@types/node",
-                    "mocha",
-                    "rimraf",
-                    "source-map-support"
-                ]
-            });
+        super();
+    }
 
-        this.Register(new BuildDependencies());
+    /**
+     * @inheritdoc
+     */
+    protected override get DependencyNames(): PackageDependencyCollectionOptions
+    {
+        let result = super.DependencyNames;
+
+        return {
+            ...result,
+            devDependencies: [
+                ...(result.devDependencies ?? []),
+                "@types/mocha",
+                "@types/node",
+                "mocha",
+                "rimraf",
+                "source-map-support"
+            ]
+        };
     }
 }
