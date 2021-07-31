46e2d0ea243b5f2bfb2901f440e7494806789ce8
Manuel Thalmann <m@nuth.ch>
2021-07-31 18:14:45 +0200

    Allow dumping `Document`s in `YAMLFileMappingTester`

--- packages/generator-ts-project-test/src/YAMLFileMappingTester.ts
+++ packages/generator-ts-project-test/src/YAMLFileMappingTester.ts
@@ -1,5 +1,5 @@
 import { GeneratorOptions, IFileMapping, IGenerator, IGeneratorSettings } from "@manuth/extended-yo-generator";
-import { YAMLConverter } from "@manuth/generator-ts-project";
+import { IDumper, YAMLConverter } from "@manuth/generator-ts-project";
 import { Document } from "yaml";
 import { ConvertibleFileMappingTester } from "./ConvertibleFileMappingTester";
 
@@ -40,5 +40,63 @@ export class YAMLFileMappingTester<TGenerator extends IGenerator<TSettings, TOpt
     public get Converter(): YAMLConverter
     {
         return new YAMLConverter();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public override get Dumper(): IDumper<Array<Document | Document.Parsed>>
+    {
+        return this.Converter;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param data
+     * The data to dump.
+     *
+     * @returns
+     * A {@link String `string`} representing the specified {@link data `data`}.
+     */
+    public override Dump(data: Array<Document | Document.Parsed>): string
+    {
+        return this.Dumper.Dump(data);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param data
+     * The data to dump to the file with the specified {@link fileName `fileName`}.
+     *
+     * @param fileName
+     * The name of the file to dump the specified {@link data `data`} to.
+     */
+    public override async DumpFile(data: Array<Document | Document.Parsed>, fileName: string): Promise<void>
+    {
+        return super.WriteFile(this.Dump(data), fileName);
+    }
+
+    /**
+     * Dumps the specified {@link data `data`} to the source-file.
+     *
+     * @param data
+     * The data to dump.
+     */
+    public override async DumpSource(data: Array<Document | Document.Parsed>): Promise<void>
+    {
+        return this.WriteSource(this.Dump(data));
+    }
+
+    /**
+     * Dumps the specified {@link data `data`} to the output-file.
+     *
+     * @param data
+     * The data to dump.
+     */
+    public override async DumpOutput(data: Array<Document | Document.Parsed>): Promise<void>
+    {
+        return this.WriteOutput(this.Dump(data));
     }
 }
