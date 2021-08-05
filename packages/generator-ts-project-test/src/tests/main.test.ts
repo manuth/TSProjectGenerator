f010768d80336e8227d8341e3b65acde66a6de38
Manuel Thalmann <m@nuth.ch>
2021-08-05 20:58:30 +0200

    Add tests for the `ParsableFileMappingTester` class

--- packages/generator-ts-project-test/src/tests/main.test.ts
+++ packages/generator-ts-project-test/src/tests/main.test.ts
@@ -1,8 +1,10 @@
 import { ConvertibleFileMappingTesterTests } from "./ConvertibleFileMappingTester.test";
+import { ParsableFileMappingTesterTests } from "./ParsableFileMappingTester.test";
 
 suite(
     "TSProjectGeneratorTest",
     () =>
     {
+        ParsableFileMappingTesterTests();
         ConvertibleFileMappingTesterTests();
     });
