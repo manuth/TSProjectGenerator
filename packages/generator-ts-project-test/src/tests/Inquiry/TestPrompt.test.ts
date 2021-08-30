ab7d31d830d4fb56d2494d44a7a10e52705686b2
Manuel Thalmann <m@nuth.ch>
2021-08-30 14:55:49 +0200

    Add tests reflecting the changes

---
+++ packages/generator-ts-project-test/src/tests/Inquiry/TestPrompt.test.ts
@@ -0,0 +1,73 @@
+import { strictEqual } from "assert";
+import { createInterface, Interface } from "readline";
+import { Question } from "inquirer";
+import { MockSTDIN, stdin } from "mock-stdin";
+import MuteStream = require("mute-stream");
+import { Random } from "random-js";
+import { TestPrompt } from "../../Inquiry/TestPrompt";
+
+/**
+ * Registers tests for the {@link TestPrompt `TestPrompt`} class.
+ */
+export function TestPromptTests(): void
+{
+    suite(
+        nameof(TestPrompt),
+        () =>
+        {
+            let random: Random;
+            let prompt: TestPrompt;
+            let mockedStdin: MockSTDIN;
+            let readLine: Interface;
+            let expectedResult: string;
+
+            suiteSetup(
+                () =>
+                {
+                    random = new Random();
+                });
+
+            setup(
+                () =>
+                {
+                    let muteStream = new MuteStream();
+                    muteStream.pipe(process.stdout);
+                    mockedStdin = stdin();
+                    expectedResult = random.string(10);
+
+                    readLine = createInterface(
+                        {
+                            terminal: true,
+                            input: process.stdin,
+                            output: muteStream
+                        });
+
+                    prompt = new TestPrompt(
+                        {
+                            name: random.string(5),
+                            default: expectedResult
+                        },
+                        readLine,
+                        {});
+                });
+
+            teardown(
+                () =>
+                {
+                    readLine.close();
+                    mockedStdin.restore();
+                });
+
+            suite(
+                nameof<TestPrompt>((prompt) => prompt.run),
+                () =>
+                {
+                    test(
+                        `Checking whether the prompt resolves to the prompt's \`${nameof<Question>((q) => q.default)}\`-option…`,
+                        async () =>
+                        {
+                            strictEqual(await prompt.run(), expectedResult);
+                        });
+                });
+        });
+}
--- packages/generator-ts-project-test/src/tests/main.test.ts
+++ packages/generator-ts-project-test/src/tests/main.test.ts
@@ -1,15 +1,19 @@
 import { ConvertibleFileMappingTesterTests } from "./ConvertibleFileMappingTester.test";
 import { DependencyCollectionTesterTests } from "./DependencyCollectionTester.test";
+import { InquiryTests } from "./Inquiry";
 import { NPMIgnoreFileMappingTesterTests } from "./NPMIgnoreFileMappingTester.test";
 import { PackageFileMappingTesterTests } from "./PackageFileMappingTester.test";
 import { ParsableFileMappingTesterTests } from "./ParsableFileMappingTester.test";
 import { ParsedFileMappingTesterTests } from "./ParsedFileMappingTester.test";
+import { TestContextTests } from "./TestContext.test";
 import { TypeScriptFileMappingTesterTests } from "./TypeScriptFileMappingTester.test";
 
 suite(
     "TSProjectGeneratorTest",
     () =>
     {
+        InquiryTests();
+        TestContextTests();
         ParsableFileMappingTesterTests();
         ConvertibleFileMappingTesterTests();
         ParsedFileMappingTesterTests();
