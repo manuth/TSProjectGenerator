d955331db4b4e2a47cdd1d3b70707450fada453b
Manuel Thalmann <m@nuth.ch>
2022-09-08 20:32:55 +0200

    Add tests for importing compiled `.ts`-files

--- packages/generator-ts-project-test/src/tests/TypeScriptFileMappingTester.test.ts
+++ packages/generator-ts-project-test/src/tests/TypeScriptFileMappingTester.test.ts
@@ -1,9 +1,12 @@
-import { strictEqual } from "node:assert";
+import { doesNotReject, doesNotThrow, strictEqual } from "node:assert";
+import { createRequire } from "node:module";
 import { GeneratorOptions, IGenerator, IGeneratorSettings } from "@manuth/extended-yo-generator";
 import { TestContext, TestGenerator } from "@manuth/extended-yo-generator-test";
 import { TypeScriptCreatorMapping } from "@manuth/generator-ts-project";
+import { PackageType } from "@manuth/package-json-editor";
 import { TempFile } from "@manuth/temp-files";
 import { SourceFile } from "ts-morph";
+import { ICompilationResult } from "../TypeScript/ICompilationResult.js";
 import { TypeScriptFileMappingTester } from "../TypeScriptFileMappingTester.js";
 
 /**
@@ -62,6 +65,20 @@ export function TypeScriptFileMappingTesterTests(): void
                 {
                     return super.NodeRequire;
                 }
+
+                /**
+                 * @inheritdoc
+                 *
+                 * @param esModule
+                 * A value indicating whether the underlying file should be compiled as an ESModule.
+                 *
+                 * @returns
+                 * An object containing information about the compilation.
+                 */
+                public override Compile(esModule: boolean): Promise<ICompilationResult>
+                {
+                    return super.Compile(esModule);
+                }
             }
 
             let transformer: TestTypeScriptCreatorMapping["Transform"];
@@ -83,15 +100,43 @@ export function TypeScriptFileMappingTesterTests(): void
                             Suffix: ".ts"
                         });
 
-                    transformer = async (sourceFile) => sourceFile;
+                    transformer = async (sourceFile) =>
+                    {
+                        sourceFile.addExportAssignment(
+                            {
+                                expression: JSON.stringify(testValue),
+                                isExportEquals: false
+                            });
+
+                        return sourceFile;
+                    };
+
                     fileMapping = new TestTypeScriptCreatorMapping(generator);
                     tester = new TestTypeScriptFileMappingTester(generator, fileMapping);
+                    await tester.Run();
                 });
 
             teardown(
                 () =>
                 {
                     outputFile.Dispose();
+                });
+
+            suite(
+                nameof<TestTypeScriptFileMappingTester>((tester) => tester.Compile),
+                () =>
+                {
+                    test(
+                        `Checking whether files can be converted to both \`${nameof(PackageType.ESModule)}\` and \`${nameof(PackageType.CommonJS)}\`…`,
+                        async () =>
+                        {
+                            let result = await tester.Compile(true);
+                            await doesNotReject(() => import(result.FileName));
+                            result.TempDirectory.Dispose();
+                            result = await tester.Compile(false);
+                            doesNotThrow(() => createRequire(import.meta.url)(result.FileName));
+                            result.TempDirectory.Dispose();
+                        });
                 });
 
             suite(
@@ -131,6 +176,34 @@ export function TypeScriptFileMappingTesterTests(): void
                             this.slow(15 * 1000);
                             await tester.Require();
                             strictEqual(Object.keys(tester.NodeRequire.cache).length, 0);
+                        });
+                });
+
+            suite(
+                nameof<TestTypeScriptFileMappingTester>((tester) => tester.Import),
+                () =>
+                {
+                    test(
+                        "Checking whether files can be imported…",
+                        async function()
+                        {
+                            this.timeout(30 * 1000);
+                            this.slow(15 * 1000);
+                            strictEqual((await tester.Import()).default, testValue);
+                        });
+                });
+
+            suite(
+                nameof<TestTypeScriptFileMappingTester>((tester) => tester.ImportDefault),
+                () =>
+                {
+                    test(
+                        "Checking whether the default export of files can be determined…",
+                        async function()
+                        {
+                            this.timeout(30 * 1000);
+                            this.slow(15 * 1000);
+                            strictEqual(await tester.ImportDefault(), testValue);
                         });
                 });
         });
